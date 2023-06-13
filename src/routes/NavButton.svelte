c55d9aee3161b9ecad63fec521114764aec6389a
Anders <dom.galbavy@gmail.com>
2023-06-14 01:56:29 +0200

    started working on navbuttons

---
+++ src/routes/NavButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	export let route: string
+	export let img: string
+	export let alt: string
+
+	let imgsrc: string
+
+	onMount(async () => {
+		imgsrc = await import('$lib/assets/' + img)
+	})
+</script>
+
+<div>
+	<a href={route}>
+		<img src={imgsrc} {alt} />
+	</a>
+</div>
