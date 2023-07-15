89781f8f0321c11908abfd651b5b07380b29a0aa
Anders142536 <anders142536@gmail.com>
2023-07-15 18:50:51 +0200

    added icon button and viewlayout component

---
+++ src/routes/IconButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label: string = ''
+	export let icon: string
+	export let extension: string = 'svg'
+</script>
+
+<button class="button">
+	<img src="/icons/{icon}.{extension}" alt={icon} />
+	{label}
+</button>
+
+<style>
+</style>
---
+++ src/routes/ViewLayout.svelte
@@ -0,0 +1,25 @@
+<div class="viewlayout-flex">
+	<div class="content">
+		<slot name="content" />
+	</div>
+	<div class="toolbar">
+		<slot name="toolbar" />
+	</div>
+</div>
+
+<style>
+	.viewlayout-flex {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+	.content {
+		flex: 1;
+	}
+
+	.toolbar {
+		height: fit-content;
+		display: flex;
+		justify-content: space-around;
+	}
+</style>
