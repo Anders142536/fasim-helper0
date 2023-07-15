4ef9ba69b5644c8c54aa87be236741cb35e7d021
Anders142536 <anders142536@gmail.com>
2023-07-15 17:23:44 +0200

    icons now work for navigation and it sticks to bottom now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,36 @@
 <script lang="ts">
-	import NavButton from "./NavButton.svelte";
+	import NavButton from './NavButton.svelte'
 </script>
 
-<div class='main-wrap'>
-<div class="main">
-	<div class='content'>
-	<slot />
-</div>
-	<nav>
-		<NavButton img='nav-cal' alt='calendar' route='/calendar' />
-		<a href="/todo">Todo</a>
-		<a href="/packs">Packs</a>
-		<a href="/settings">Settings</a>
-	</nav>
-</div>
+<div class="main-wrap">
+	<div class="main">
+		<div class="content">
+			<slot />
+		</div>
+		<nav>
+			<NavButton img="nav-cal" alt="calendar" route="/calendar" />
+			<NavButton img="nav-todo" alt="todo" route="/todo" />
+			<NavButton img="nav-packs" alt="packs" route="/packs" />
+			<NavButton img="nav-set" alt="settings" route="/settings" />
+		</nav>
 	</div>
-
+</div>
 
 <style>
 	.main-wrap {
 		display: flex;
 		width: 100vw;
 		justify-content: center;
+		height: 100%;
 	}
 
 	.main {
 		display: flex;
 		flex-direction: column;
-		height: max(600px, fit-content);
 		min-height: 300px;
 		max-height: 100vh;
 		min-width: 400px;
 		max-width: max(800px, 100vw);
-
 	}
 
 	.content {
@@ -42,9 +40,8 @@
 	nav {
 		display: flex;
 		flex-direction: row;
-		justify-content: space-around;
+		justify-content: space-evenly;
 		gap: 10px;
 		height: 64px;
 	}
-
 </style>
--- src/routes/NavButton.svelte
+++ src/routes/NavButton.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-
 	export let route: string
 	export let img: string
 	export let alt: string
-
-	let imgsrc: string
-
-	onMount(async () => {
-		imgsrc = await import(`$lib/assets/${img}.png`)
-		console.log(`imgsrc: ${imgsrc}`)
-	})
 </script>
 
 <div>
 	<a href={route}>
-		<img src={imgsrc} {alt} />
+		<img src="/icons/{img}.png" {alt} />
 	</a>
 </div>
