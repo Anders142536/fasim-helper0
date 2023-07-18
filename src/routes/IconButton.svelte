f8ce8fbcac11e31fa28f603f048f2c7caadc272f
Anders142536 <anders142536@gmail.com>
2023-07-18 02:03:36 +0200

    added hero icons and played around with the best way to add them

--- src/routes/IconButton.svelte
+++ src/routes/IconButton.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
 	export let label: string = ''
-	export let icon: string
-	export let extension: string = 'svg'
 </script>
 
-<button class="button">
-	<img src="/icons/{icon}.{extension}" alt={icon} />
-	{label}
+<button>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+	>
+		<slot />
+	</svg>
+	<p>{label}</p>
 </button>
 
 <style>
+	button {
+		background: none;
+		border: none;
+		color: var(--text);
+		display: flex;
+		gap: 4px;
+		align-items: center;
+		width: fit-content;
+	}
+
+	svg {
+		width: 1.5em;
+		height: 1.5em;
+	}
+
+	p {
+		width: max-content;
+	}
 </style>
--- src/routes/packs/+page.svelte
+++ src/routes/packs/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import ViewLayout from '../ViewLayout.svelte'
 	import IconButton from '../IconButton.svelte'
+	import ArchiveBoxX from '$lib/svg/ArchiveBoxX.svelte'
 </script>
 
 <ViewLayout>
 	<h1 slot="content">Packs</h1>
 
 	<svelte:fragment slot="toolbar">
-		<IconButton icon="plus" label="Add pack" />
-		<IconButton icon="trash" label="History" />
+		<IconButton label="Add pack">
+			<ArchiveBoxX />
+		</IconButton>
+		<IconButton label="History">
+			<ArchiveBoxX />
+		</IconButton>
 	</svelte:fragment>
 </ViewLayout>
