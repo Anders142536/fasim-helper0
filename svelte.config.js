98a3f9fd250fb62a61501a44dc158a0ec29b7bee
Anders142536 <anders142536@gmail.com>
2023-10-15 16:10:37 +0200

    added skeleton and tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,14 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
+
+	vitePlugin: {
+		inspector: true,
+	},
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
