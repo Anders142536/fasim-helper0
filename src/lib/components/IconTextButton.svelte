0b930fa7046a7eaf1cb3a3d792b2ba5ddd260a2c
Anders142536 <anders142536@gmail.com>
2023-10-28 21:16:09 +0200

    icontextbutton visibility can now be toggled

--- src/lib/components/IconTextButton.svelte
+++ src/lib/components/IconTextButton.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	export let label: string | null = null
 	export let onClick: () => void
+	export let visible = true
 </script>
 
 <button 
 	on:click={onClick}
-	class={label === null ? 'btn-icon btn-icon-sm' : 'btn'}>
+	class={label === null ? 'btn-icon btn-icon-sm' : 'btn'}{visible ? '' : ' invisible'}>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		class='w-6 h-6'
