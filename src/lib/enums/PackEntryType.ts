694f2f714eee8a746537667d2e6852b55ccad495
Anders142536 <anders142536@gmail.com>
2023-10-29 03:21:51 +0100

    added further pack entry types

--- src/lib/enums/PackEntryType.ts
+++ src/lib/enums/PackEntryType.ts
@@ -1,7 +1,19 @@
 export enum PackEntryType {
 	UNKNOWN = 'Unknown',
+	OTHER = 'Other',
 	TRACTOR = 'Tractor',
 	HARVESTER = 'Harvester',
-	SEEDER = 'Seeder'
+	SEEDER = 'Seeder',
+	TRAILER = 'Trailer',
+	CAR = 'Car',
+	MOWER = 'Mower',
+	FORKLIFT = 'Forklift',
+	FORESTRY_MACHINE = 'Forestry Machine',
+	PLOW = 'Plow',
+	CULTIVATOR = 'Cultivator',
+	WEEDER = 'Weeder',
+	TRUCK = 'Truck',
+	ANIMAL = 'Animal',
+	BUILDING = 'Building'
 	// TODO: add others
 }
